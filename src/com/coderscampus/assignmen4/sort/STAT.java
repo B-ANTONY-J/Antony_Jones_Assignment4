881352eef0e73148f8997b7b2597d0df369519c6
B-ANTONY-J <antony.jones14@gmail.com>
2024-07-14 13:35:51 -0400

    subclasses extended to POJO

--- src/com/coderscampus/assignmen4/sort/STAT.java
+++ src/com/coderscampus/assignmen4/sort/STAT.java
@@ -1,5 +1,14 @@
 package com.coderscampus.assignmen4.sort;
 
-public class STAT {
+public class STAT extends Sorted{
 
+//	public STAT(String id, String name, String course, int grade) {
+//		super(id, name, course, grade);
+//		// TODO Auto-generated constructor stub
+//	}
+	public STAT(String id, String name, int grade) {
+        // Call the Sorted constructor with hardcoded "STAT" for the course
+        new Sorted(id, name, "STAT", grade);
+    }
 }
+
